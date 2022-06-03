163f34a3a772a0029f07f6a640f10ede015c8957
Lenard <lenard.b19@gmail.com>
2022-06-03 08:35:31 +0200

    simple comments

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
+
+# -- Database Connection --
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
 
+# -- Flask Application Configuragtions --
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'makethesecretkeywhateveryouwant'
@@ -13,12 +16,17 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
+
+    # Enable Cross-Origin Ressource Sharing for local frontend server only
     CORS(app, resources={r"/*":{'origins': "http://localhost:8080", "allow_headers": "Access-Control-Allow-Origin"}})
 
+
+    #  Referencing all routes defined in seperate files
     from .endpoints import endpoints
     from .security import security
 
+    # Defining prefixes for routes
     app.register_blueprint(endpoints, url_prefix='/')
-    app.register_blueprint(security, url_prefix='/auth/api/v1')
+    app.register_blueprint(security, url_prefix='/auth')
 
     return app
--- backend/endpoints.py
+++ backend/endpoints.py
@@ -2,6 +2,8 @@ from flask import Blueprint
 
 endpoints = Blueprint('endpoints', __name__)
 
+
+# Defining routes
 @endpoints.route('/')
 def home():
     return "<h1>Hompage</h1>"
