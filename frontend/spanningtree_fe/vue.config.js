94c19aba2a637bac4cb3588fa57b348bdd88c55a
Lenard <lenard.b19@gmail.com>
2022-06-04 12:30:39 +0200

    rmvd dev server since CORS enabled

--- frontend/spanningtree_fe/vue.config.js
+++ frontend/spanningtree_fe/vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    proxy: 'https://localhost:5000/'
-  }
+  transpileDependencies: true
 });
