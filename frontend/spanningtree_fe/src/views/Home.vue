37556e382c0e8672903605de14823695482d982d
Lenard <lenard.b19@gmail.com>
2022-06-04 19:49:44 +0200

    BE gives correct response to tree sent by FE

--- frontend/spanningtree_fe/src/views/Home.vue
+++ frontend/spanningtree_fe/src/views/Home.vue
@@ -7,11 +7,15 @@
         <addEdgeListVue @infoPopup="$emit('infoPopup', $event)" :newVrtxList="changedList"/>
     </div>
 </div>
+<div class="send-wrapper">
+    <button class="btn btn primary btn-success btn-lg" @click="sendTree()">Submit Spanning Tree to Compute</button>
+</div>
 </template>
 
 <script>
 import addVrtxListVue from '@/components/addVrtxList.vue'
 import addEdgeListVue from '@/components/addEdgeList.vue'
+import axios from 'axios'
 
 export default {
     name: "HomePage",
@@ -28,6 +32,14 @@ export default {
     methods: {
         passListChanged() {
             this.changedList = JSON.parse(localStorage.getItem('vrtxList'))
+        },
+        async sendTree() {
+            let payload = {
+                'vrtcs': JSON.parse(localStorage.getItem('vrtxList')),
+                'edges': JSON.parse(localStorage.getItem('edgeList'))
+            }
+            const response = await axios.post('computeTree', payload)
+            console.log("response", response);
         }
     }
 }
@@ -40,7 +52,22 @@ export default {
     justify-content: space-between;
 }
 .component-wrapper {
-    width: 49%;
+    width: 50%;
     margin: 5px;
+    background-color: rgb(236, 236, 236);
+    border-radius: 5px;
+    padding: 5px;
+}
+
+.component-wrapper:first-of-type {
+    margin-right: 2px;
+}
+
+.component-wrapper:last-of-type {
+    margin-left: 2px;
+}
+
+.send-wrapper {
+    margin: 50px auto;
 }
 </style>
